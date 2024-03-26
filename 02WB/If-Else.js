3f902eec1736ce99b678105018511ecfe7634abc
aFemzz <andhika.mailss@gmail.com>
2024-03-26 20:15:32 +0700

    first commit

---
+++ 02WB/If-Else.js
@@ -0,0 +1,10 @@
+let nama = "joni";
+let nilai = 90;
+let absen = 2;
+
+if (nilai >= 70 && absen < 5){
+    console.log("Lulus");
+}
+else{
+    console.log("Tidak Lulus");
+}
---
+++ 02WB/Operator.js
@@ -0,0 +1,12 @@
+let angka1 = 512;
+let angka2 = 512;
+
+if (angka2 > angka1 ){
+    console.log("True")
+}
+else if (angka1 == angka2){
+    console.log("-1")
+}
+else{
+    console.log("False")
+}
